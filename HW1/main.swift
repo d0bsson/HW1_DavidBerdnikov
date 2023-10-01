9e43a7eef3b38c31dc58fa6337666faf01e4def6
David <ber.dev@yandex.ru>
2023-10-01 23:39:24 +0300

    Четвертое ДЗ

--- HW1/main.swift
+++ HW1/main.swift
@@ -7,4 +7,27 @@
 
 import Foundation
 
-print("Hello, !")
+//a. Добавление события:
+var events = [String: [String]]()
+
+func insertEvent(date: String, event: String) {
+    
+    if events.index(forKey: date) == nil {
+        events[date] = [event]
+    }
+    print(events)
+}
+
+insertEvent(date: "22/02/2022", event: "NewYear")
+insertEvent(date: "22/04/2023", event: "H-day")
+
+//b. Отображение событий:
+
+func appDict(date: String) {
+    print("Выбирете функцию:\nДобавить событие\nПоказать событие в эту дату\nВыйти")
+    readLine()
+    
+    
+}
+
+appDict(date: "22/02/2023")
