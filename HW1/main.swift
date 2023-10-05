c6414e6626164b9a6d794e647ec11b122c8fcad9
David <ber.dev@yandex.ru>
2023-10-05 23:10:10 +0300

    Сделал 7 заданий

--- HW1/main.swift
+++ HW1/main.swift
@@ -75,3 +75,60 @@ enum PaymentStatus {
     
 }
 
+
+// 4 -  Задание: Рекомендации по режимам камеры
+
+enum CameraMode: String {
+    case auto = "Советуем если совсем не шаришь в фото"
+    case portret = "Классный режим для размытия фона, для инсты пойдет"
+    case landscape = "Если честно, сам не знаю, может для природы"
+    case night = "Если темненько вокруг - выбор твой"
+}
+
+func recCameraMode(cameraMode: CameraMode) {
+    print(cameraMode.rawValue)
+}
+
+recCameraMode(cameraMode: .landscape)
+
+
+// 5 - Задание: Диеты животных в зоопарке
+
+enum AnimalType: String {
+    case predator = "Это Хищник. Ест всех подряд."
+    case herbivore = "Это травоядное. Есть травушку"
+    case insect = "Это насекомое. Ест листики"
+}
+
+func whatEatAnimal(animal: AnimalType) {
+    print(animal.rawValue)
+}
+whatEatAnimal(animal: .insect)
+
+// 6 - Задание: Отслеживание статуса заказа в ресторане
+
+enum OrderStatus: String, CaseIterable {
+    case accepted = "Ваш заказ принят. Скоро будет готовиться"
+    case prepared = "Ваш заказ готовится. Скоро будет готов к выдаче"
+    case readyToServe = "Ваш заказ готов к выдаче. Скоро будет доставлен"
+    case delivered = "Ваш заказ доставляется. Ожидайте"
+}
+
+func nextOrderStatus(orderStatusNow: OrderStatus) {
+    switch orderStatusNow {
+    case .accepted: print("Следующий статус готовится")
+    case .prepared: print("Следующий статус готов к выдаче")
+    case .readyToServe: print("Следующий статус доставляется")
+    case .delivered: print("Ваш заказ скоро будет доставлен")
+    }
+}
+
+nextOrderStatus(orderStatusNow: .prepared)
+
+//7 - Задание: Информация о номерах в отеле
+
+enum RoomType {
+    case oneBedRoom
+    case twoBedRoom
+    case luxury
+}
